61e839e7dc6d943440ce17b33e1c521d8dc74b87
chen.liang <cl123456>
2018-12-14 15:57:31 +0800

    初始化工程

---
+++ src/main/java/com/example/uml/UmlApplication.java
@@ -0,0 +1,14 @@
+package com.example.uml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmlApplication.class, args);
+	}
+
+}
+
